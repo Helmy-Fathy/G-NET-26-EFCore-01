7af96112d7cd630aa249af77269747dd73893b5c
Helmy-Fathy <7elmyfathy@gmail.com>
2026-04-21 03:02:58 +0200

    install the required packages to Use Sql Server as the database provider

--- G-NET-26-EFCore-01/Program.cs
+++ G-NET-26-EFCore-01/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*  CASE STUDY: BOOKSTORE
+                You are hired as a junior developer at &quot;ReadMore Books&quot;, a
+                small bookstore that
+                needs a simple database system. The owner explains: &quot;We
+                need to track our book
+                inventory - each book has a title, ISBN number, price, number
+                of pages, the year
+                it was published, and whether it&#39;s currently in stock. We also
+                want to keep
+                information about the authors we work with, including their
+                first and last names,
+                email addresses, a short biography, and their date of birth.
+                Finally, we organize
+                our books into different categories - each category has a
+                name, description, and
+                we need to know if the category is still active or not.&quot;
+                --------------------------------------------------------------------
+                Requirements:
+                1. Create a Console Application using Entity Framework Core
+                2. Use EF Core CONVENTIONS ONLY
+                3. Use Sql Server as the database provider
+                4. Identify and create the necessary entities from the case study
+                5. Create an appropriate DbContext class
+                6. Demonstrate that the database is created correctly
+            */
         }
     }
 }
